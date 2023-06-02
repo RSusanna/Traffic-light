9cce2280099fc203a642500545fed74e55f99388
Susanna <susanna1235@gmail.com>
2023-06-02 16:01:18 +0300

    I have learned how to create circles from UIImageView and work with the brightness - alpha. Now I need to make it so that when the 'START' button is pressed, the color turns red and the button title changes to 'NEXT'. Subsequent presses of the 'NEXT' button should change the active color to the next one in order. Therefore, each subsequent press of the 'Next' button should sequentially change the traffic light signals.

--- Traffic-light/Traffic-light/ViewController.swift
+++ Traffic-light/Traffic-light/ViewController.swift
@@ -2,18 +2,37 @@
 //  ViewController.swift
 //  Traffic-light
 //
-//  Created by Susanna Zakaryan on 29.05.2023.
+//  Created by Susanna R on 29.05.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIImageView!
+    
+    @IBOutlet weak var yellowLight: UIImageView!
+    
+    @IBOutlet weak var greenLight: UIImageView!
+    
+    @IBOutlet weak var pressToSwitchColor: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Настройка начальной яркости UIImageView
+        redLight.alpha = 1.0
+        yellowLight.alpha = 1.0
+        greenLight.alpha = 1.0
+        //    }
+        func buttonTapped(_ sender: UIButton) {
+            // Изменение яркости в зависимости от состояния кнопки
+            if redLight.alpha == 1.0 {
+                redLight.alpha = 0.2
+            } else {
+                redLight.alpha = 1.0
+            }
+        }
+        
     }
-
-
 }
-
